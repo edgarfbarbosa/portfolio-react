1cfe95e818326f36a7da9c2fdb999625beb842b6
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-03-13 22:12:39 -0300

    add: pasta icons e desenvolvimento do footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,13 +1,14 @@
 import './footer.css'
 
+import GitHubIcon from '../../assets/img/icons/github.svg'
+import LinkedInIcon from '../../assets/img/icons/linkedin.svg'
+
 const Footer = () => {
     return (
-        <footer>
+        <footer className="footer__container">
             <div>
-                <ul>
-                    <li>LinkedIn</li>
-                    <li>GitHub</li>
-                </ul>
+                <img src={LinkedInIcon}></img>
+                <img src={GitHubIcon}></img>
             </div>
         </footer>
     )
