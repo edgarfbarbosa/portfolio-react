c6930835da605e4a3bf812fc5feb3b5dc5054662
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-03-26 14:23:08 -0300

    adicionado foto para a seção home

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -6,9 +6,14 @@ import jsIcon from '../../assets/img/icons/javascript.svg'
 import tsIcon from '../../assets/img/icons/typescript.svg'
 import reactIcon from '../../assets/img/icons/react.svg'
 
+import profilePhoto from '../../assets/img/profile.jpeg'
+
 const Home = () => {
     return (
         <section className="home__container">
+            {/* <div className="img__container">
+                <img src={profilePhoto} className="img__photo"></img>
+            </div> */}
             <div className="home__content">
                 <h1>Edgar Faria Barbosa</h1>
                 <h2>Front-end Developer</h2>
@@ -16,9 +21,6 @@ const Home = () => {
                     Sou estudante de Engenharia de Software e estou me especializando em Desenvolvimento Front-end.
                 </p>
             </div>
-            {/* <div>
-                <img></img>
-            </div> */}
             <div className="tech__container">
                 <h3>Tech Stack |</h3>
                 <div>
