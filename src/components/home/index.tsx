2233a8e1c98ef58520f1d6cedc5328f998cfac56
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-04-03 20:22:57 -0300

    ajustando a seção home

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -3,15 +3,13 @@ import './home.css'
 const Home = () => {
     return (
         <section className="home__container">
-            <div className="home__content">
+            <div className="home__content--container">
                 <h1 className="home__title">Edgar Faria Barbosa</h1>
                 <h2 className="home__subtitle">Front-end Developer</h2>
-                <p>
+                <p className="home__phrase">
                     Sou estudante de Engenharia de Software e estou me especializando em Desenvolvimento Front-end.
                 </p>
-            </div>
-            <div>
-                <button>Currículo</button>
+                <button className="home__btn">Currículo</button>
             </div>
         </section>
     )
